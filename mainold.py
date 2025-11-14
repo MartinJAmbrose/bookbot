04be00b54dfa0b121c05a7b4f94133e6b621f73e
MartinJAmbrose <martin.ambrose@gmail.com>
2025-11-14 11:18:45 -0500

    A: Main Read Frankenstein

---
+++ mainold.py
@@ -0,0 +1,12 @@
+#  Read Frankenstein.txt file and print output
+def get_book_text(filepath):
+    with open(filepath, "r", encoding="utf-8") as f:
+    return f.read()
+
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+        print(text)
+
+    if __name__ == "__main__"
+        main()
